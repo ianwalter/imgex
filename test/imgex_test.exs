f7e726f7bbf5f8a6836ac33a7e1bfaeafc36d2ec
Jason Axelson <jason@hobnob.io>
2017-12-20 15:06:16 -1000

    Handle an empty map as `params`
    Remove support for params with value `nil` since no one is probably
    sending in `nil` anyway.
    
    An empty map matches as the default value for `params` since otherwise
    `params` will be a map. Without this change url's will be generated like:
    http://my-bucket.imgix.net/image_uploads/ba2b0236-619f-42a8-a7ff-25ac678a755d/image.jpg?&s=31c0b04f75b81ada770c91d652099698
    
    Note the `image.jpg?&s=` instead of `image.jpg?s=`

--- test/imgex_test.exs
+++ test/imgex_test.exs
@@ -1,4 +1,14 @@
 defmodule ImgexTest do
   use ExUnit.Case
   doctest Imgex
+
+  test "proxy_url/2 when params are an empty map generates an appropriate url" do
+    assert Imgex.proxy_url("http://avatars.com/john-smith.png", %{}) ==
+      "https://my-social-network.imgix.net/http%3A%2F%2Favatars.com%2Fjohn-smith.png?s=493a52f008c91416351f8b33d4883135"
+  end
+
+  test "url/2 when params are an empty map generates an appropriate url" do
+    assert Imgex.url("/images/jets.png", %{}) ==
+      "https://my-social-network.imgix.net/images/jets.png?s=7c6a3ef8679f4965f5aaecb66547fa61"
+  end
 end
